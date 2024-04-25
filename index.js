5d5bace264ea69c520befe88c847bb872d6b8267
k2876 <cass1025@naver.com>
2024-04-25 23:47:14 +0900

    수정

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+var thumbnail = document.querySelector("#pic");
+var playButton = document.querySelector("#play");
 var video = document.querySelector("#video");
-video === null || video === void 0 ? void 0 : video.addEventListener("click", function (e) {
+var progressBar = document.querySelector("#progress");
+var timeDisplay = document.querySelector("#time");
+var button = document.querySelector("#button");
+var progressbarContainer = document.querySelector("#progressbar-container");
+playButton.addEventListener("click", function () {
+    thumbnail.style.display = "none"; // 이미지 숨김
+    playButton.style.display = "none"; // 이미지 숨김
+    progressbarContainer.style.display = "flex";
+    video.src = "./images/video.mp4"; // 비디오 소스 설정
+    video.play(); // 비디오 자동 재생
+});
+video.addEventListener("click", function () {
     if (video === null || video === void 0 ? void 0 : video.paused) {
         video === null || video === void 0 ? void 0 : video.play();
     }
@@ -7,7 +20,32 @@ video === null || video === void 0 ? void 0 : video.addEventListener("click", fu
         video === null || video === void 0 ? void 0 : video.pause();
     }
 });
-setTimeout(function () {
-    // console.log(video?.duration);
-    console.log(video.src);
-}, 2000);
+button.addEventListener("click", function () {
+    if (video === null || video === void 0 ? void 0 : video.paused) {
+        button.textContent = "멈춤";
+        video === null || video === void 0 ? void 0 : video.play();
+    }
+    else {
+        button.textContent = "재생";
+        video === null || video === void 0 ? void 0 : video.pause();
+    }
+});
+progressBar.addEventListener("click", function (e) {
+    var pos = (e.pageX - progressBar.getBoundingClientRect().left) /
+        progressBar.offsetWidth;
+    video.currentTime = pos * video.duration;
+});
+video.addEventListener("timeupdate", function () {
+    var progress = (video.currentTime / video.duration) * 100;
+    progressBar.style.width = "".concat(progress, "%");
+    var formatTime = function (time) {
+        var minutes = Math.floor(time / 60);
+        var seconds = Math.floor(time % 60);
+        var result = "".concat(minutes, ":").concat(seconds);
+        return result;
+    };
+    // 현재 재생 시간 및 전체 영상 시간 업데이트
+    var currentTime = formatTime(video.currentTime);
+    var duration = formatTime(video.duration);
+    timeDisplay.textContent = "".concat(currentTime, " / ").concat(duration);
+});
--- index.ts
+++ index.ts
@@ -1,13 +1,57 @@
+const thumbnail = document.querySelector("#pic") as HTMLImageElement;
+const playButton = document.querySelector("#play") as HTMLDivElement;
 const video = document.querySelector("#video") as HTMLVideoElement;
+const progressBar = document.querySelector("#progress") as HTMLElement;
+const timeDisplay = document.querySelector("#time") as HTMLElement;
+const button = document.querySelector("#button") as HTMLElement;
+const progressbarContainer = document.querySelector(
+  "#progressbar-container"
+) as HTMLElement;
 
-video?.addEventListener("click", (e) => {
+playButton.addEventListener("click", () => {
+  thumbnail.style.display = "none"; // 이미지 숨김
+  playButton.style.display = "none"; // 이미지 숨김
+  progressbarContainer.style.display = "flex";
+  video.src = "./images/video.mp4"; // 비디오 소스 설정
+  video.play(); // 비디오 자동 재생
+});
+
+video.addEventListener("click", () => {
   if (video?.paused) {
     video?.play();
   } else {
     video?.pause();
   }
 });
-setTimeout(() => {
-  // console.log(video?.duration);
-  console.log(video.src);
-}, 2000);
+button.addEventListener("click", () => {
+  if (video?.paused) {
+    button.textContent = "멈춤";
+    video?.play();
+  } else {
+    button.textContent = "재생";
+    video?.pause();
+  }
+});
+progressBar.addEventListener("click", function (e) {
+  const pos =
+    (e.pageX - progressBar.getBoundingClientRect().left) /
+    progressBar.offsetWidth;
+  video.currentTime = pos * video.duration;
+});
+
+video.addEventListener("timeupdate", function () {
+  const progress = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = `${progress}%`;
+
+  const formatTime = (time: number): string => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const result = `${minutes}:${seconds}`;
+    return result;
+  };
+
+  // 현재 재생 시간 및 전체 영상 시간 업데이트
+  const currentTime = formatTime(video.currentTime);
+  const duration = formatTime(video.duration);
+  timeDisplay.textContent = `${currentTime} / ${duration}`;
+});
